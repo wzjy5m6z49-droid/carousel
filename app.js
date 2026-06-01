8b1659900d6f28db7945b2ae7133117499b42491
wzjy5m6z49-droid <wzjy5m6z49@privaterelay.appleid.com>
2026-06-01 13:14:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,14 @@ const BUTTON_BACKGROUND_COLOR =
 const BUTTON_TEXT_COLOR =
   config.buttonTextColor || "#333333";
 
-const ARROW_POSITION =
-  config.arrowPosition || "inside";
+const shell =
+  document.querySelector(".carouselShell");
+
+if(ARROW_POSITION === "outside"){
+  shell.classList.add("outside");
+}else{
+  shell.classList.add("inside");
+}
 
 const slider = document.getElementById("slider");
 const dots = document.getElementById("dots");
